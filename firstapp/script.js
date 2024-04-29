fae95c49908228d5a9e7f05d85510e1335786221
Ana Luiza <anareis.pato@gmail.com>
2024-04-29 17:17:40 -0300

    Criacao de salario e calculo de salario liquido

--- firstapp/script.js
+++ firstapp/script.js
@@ -1,12 +1,20 @@
 //input
 anoNascimento = 1998;
 nome = "Ana Luiza";
+salarioBase = 1000;
+gratificacao = 500; 
+bonus = 200;
+desconto = 300;
+
+salarioLiquido = 0;
 
 //processamento
-hoje = new Date();
+hoje = new Date()
 anoAtual = hoje.getFullYear();
-idade = 2024 - anoNascimento;
-mensagem = "Sou " + nome + ", tenho " + idade + " anos.";
+idade = anoAtual - anoNascimento;
+
+salarioLiquido = salarioBase + gratificacao + bonus - desconto
 
 //output
+mensagem = "Sou " + nome + ", tenho " + idade + " anos e ganho R$" + salarioLiquido;
 alert(mensagem);
